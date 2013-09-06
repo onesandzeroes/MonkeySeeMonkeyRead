9d444706f6d0815ca80cd0d6c62d2546c2e8a8af
onesandzeroes <onesandzeroes@github.com>
2013-09-06 18:39:27 +1000

    Docstrings and tests for nltk_based_examples

--- nltk_based_examples.py
+++ nltk_based_examples.py
@@ -1,8 +1,23 @@
+"""
+Markov models of bigram character probabilities.
+Throughout this script, I'm using '^' and '$'
+as special start and end symbols.
+"""
+import doctest
 import itertools
 import nltk
 
 
 def return_chars_from_words(word_list):
+    """
+    Given a raw word list (possibly containing things like
+    punctuation, which will be stripped out), return all the
+    characters, plus start and end symbols around each word.
+
+    >>> res = list('^hello$^world$')
+    >>> list(return_chars_from_words(['hello', 'world'])) == res
+    True
+    """
     words = (w.lower() for w in word_list if w.isalpha())
     char_gen = itertools.chain(c for w in words for c in ('^' + w + '$'))
     for c in char_gen:
@@ -26,7 +41,11 @@ def generate_word(model, start_letter=None, max_length=10):
     return word[1:-1]
 
 
-moby_raw_words = nltk.corpus.gutenberg.words('melville-moby_dick.txt')
-moby_chars = list(return_chars_from_words(moby_raw_words))
+def test_moby_model():
+    moby_raw_words = nltk.corpus.gutenberg.words('melville-moby_dick.txt')
+    moby_chars = list(return_chars_from_words(moby_raw_words))
 
-bigram_model = nltk.model.NgramModel(2, moby_chars, pad_left=False)
+    bigram_model = nltk.model.NgramModel(2, moby_chars, pad_left=False)
+
+if __name__ == '__main__':
+    print(doctest.testmod())
